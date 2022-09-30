aeda9c9d3205ef60fd43eeb2ec5e39ecc8bb6167
MoonGyuBin <2019captain@gmail.com>
2022-09-30 21:41:38 +0900

    instapost create_post func add, authenticated add

--- Commelier/urls.py
+++ Commelier/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #20220930 박소민 
     path('index/', views.index),
+    path('new_post/', views.create_post), #new_post -> create_post로 변경 (22.09.30 문규빈)
 ]
