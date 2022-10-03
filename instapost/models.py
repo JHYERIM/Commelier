2dd0151d4bd0c2b1a937a8b69a9ec5e6df63ac5c
MoonGyuBin <2019captain@gmail.com>
2022-10-03 23:39:09 +0900

    index, new_post 연결하는 코드 작성 및 수정

--- instapost/models.py
+++ instapost/models.py
@@ -10,16 +10,14 @@ class Instapost(models.Model): #메인페이지 클래스 생성
     content = models.TextField(max_length=100)
     # image = models.ImageField # 이건 이미지가 딱 하나 업로드할 수 있습니다.
     create_date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey('instauser.InstaUser', on_delete=models.CASCADE)
-
+    author = models.ForeignKey(InstaUser, on_delete=models.CASCADE)
+    
 
 class Image(models.Model): # 게시물 목록/ 게시물 작성에 들어갈 이미지 업로드 클래스 생성
+   
     image = models.ImageField(upload_to='ssum')
 
-class Posting(models.Model): #게시글 작성 클래스 생성
-    content = models.TextField(max_length=100, null=True)
-    create_date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey('instauser.InstaUser', on_delete=models.CASCADE)
-
+#2022.10.03 class가 하나만 있어도 될 것 같아서 posting class 삭제
+#image -foreign key 
 
 
