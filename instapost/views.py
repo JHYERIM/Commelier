290a9ab20057d16fd1ba14719254c5fcb4e3bc5d
timeiscoal <timeiscoal@gmail.com>
2022-10-04 17:30:47 +0900

    add

--- instapost/views.py
+++ instapost/views.py
@@ -20,8 +20,8 @@ def create_post(request):
         user = request.instauser #현재 로그인 한 사용자를 불러오기
         print(user)
         new_post = Posting( ) #글쓰기 모델 가져오기
-        new_post.author = user.name # 모델에 사용자 저장 하기 위해 불러옴
+        new_post.author = user # 모델에 사용자 저장 하기 위해 불러옴
         new_post.content = request.POST.get('content','') #모델에 글 저장하기
         new_post.save()
-        return redirect(request, 'create_post') # elif가 실행되지 않으면 'create_post'로 되돌아감
+        return redirect(request, 'instapost:index') # elif가 실행되지 않으면 'create_post'로 되돌아감
     # 
