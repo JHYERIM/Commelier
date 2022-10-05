f36eb610214396d0786c05bd62f6f5b7ae698cdf
haeminchoi2 <sjhk02@gmail.com>
2022-10-05 09:17:14 +0900

    221005 최해민 프로필페이지 작업

--- instapost/models.py
+++ instapost/models.py
@@ -11,7 +11,7 @@ class Instapost(models.Model): #메인페이지 클래스 생성
     content = models.TextField(max_length=100)
     # image = models.ImageField # 이건 이미지가 딱 하나 업로드할 수 있습니다.
     create_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(InstaUser, on_delete=models.CASCADE)
+    author = models.ForeignKey(InstaUser, on_delete=models.CASCADE, related_name='instaposts')
     
 
 class Image(models.Model): # 게시물 목록/ 게시물 작성에 들어갈 이미지 업로드 클래스 생성
